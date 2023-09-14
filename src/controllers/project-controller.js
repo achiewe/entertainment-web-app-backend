f0202e64c80158d12b737f7fffd96892a8191803
achiewe <mamukashviliachi706@gmail.com>
2023-09-14 16:45:32 +0400

    add new functions fetchEntertainmentInfo for take entertainment and useInfo to create when signUp user info

--- src/controllers/project-controller.js
+++ src/controllers/project-controller.js
@@ -1,4 +1,5 @@
 import Entertainment from "../models/Entertainment.js";
+import User from "../models/userSchema.js";
 
 export const PostInfo = async (req, res) => {
   try {
@@ -7,5 +8,41 @@ export const PostInfo = async (req, res) => {
     res.json(result);
   } catch (error) {
     res.status(500).json({ message: "info cannot post" });
+  }
+};
+
+export const fetchEntertainmentInfo = async () => {
+  try {
+    const entertainment = await Entertainment.find();
+    return entertainment;
+  } catch (error) {
+    console.log(error);
+    throw new Error("error happend while get data");
+  }
+};
+
+export const UserInfo = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const utilizeEmail = await User.findOne({ email });
+
+    if (utilizeEmail) {
+      return res
+        .status(400)
+        .json({ error: "Email address is already registered" });
+    }
+
+    const entertainmentInfo = await fetchEntertainmentInfo();
+
+    const newUser = new User({
+      email,
+      password,
+      entertainmentInfo: entertainmentInfo,
+    });
+
+    await newUser.save();
+  } catch (error) {
+    res.status(500).json({ error: "happend error" });
   }
 };
